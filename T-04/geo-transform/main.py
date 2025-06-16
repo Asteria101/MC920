5389533b81983d2c5c2b0b485018ac836bbb68bd
Asteria101 <lebrito1268@gmail.com>
2025-06-16 15:55:08 -0300

    Read and process arguments

--- T-04/geo-transform/main.py
+++ T-04/geo-transform/main.py
@@ -1,57 +1,98 @@
-import numpy as np
-import cv2
-import skimage as ski
-import matplotlib.pyplot as plt
-from sys import argv
-import argparse
-
-def entry() -> argparse.Namespace:
-    """
-    Parses command line arguments for the geometric transformation program.
-
-    Input format
-    ----------
-    python3 main.py [-a angle] 
-                    [-s scale] 
-                    [-d width height] 
-                    [m interpolation] 
-                    [-i input] 
-                    [-o output]
-
-    Returns
-    -------
-    argparse.Namespace
-        An object containing the parsed command line arguments.
-    """
-
-    parser = argparse.ArgumentParser(description="Program for operating scale or rotation \
-                                     on digital images, through geometric transformations.")
-    
-    parser.add_argument("-a", type=float, default=0.0,
-                        help="Angle of rotation in degrees (default: 0.0)")
-    
-    parser.add_argument("-s", type=float, default=1.0,
-                        help="Scale factor (default: 1.0)")
-    
-    parser.add_argument("-d", type=int, nargs=2, default=[0, 0],
-                        help="Dimensions of the output image as width and height (default: 0 0)")
-    
-    parser.add_argument("-m", required=True, type=str, choices=["n", "b", "bi", "l"],
-                        help="Interpolation method: \n- n [nearest];\n- b [bilinear];\n- bi [bicubic];\n- l [lagrange polynomial].")
-    
-    parser.add_argument("-i", required=True, type=str, help="Input image file path")
-
-    return parser.parse_args()
-
+from interpolation import *
 
 def main():
-    args = entry()
+    args = read()
+    src = ski.io.imread(args.i)
 
-    src = cv2.imread(args.i)
-    plt.imshow(src)
-    plt.axis('off')
-    plt.show()
+    if len(src.shape) == 2:
+        src = src[:, :, np.newaxis]
+    h, w = src.shape[:2]
 
+    sw, sh, = args.s, args.s
+    if args.d:
+        sw = args.d[0] / w
+        sh = args.d[1] / h
+
+    angle = args.a * np.pi / 180 
+
+    cos = np.cos(angle)
+    sin = np.sin(angle)
+
+    rot_m = [[cos, sin, (1 - cos) * w / 2 - sin * h / 2],
+             [-sin, cos, sin * w / 2 + (1 - cos) * h / 2]]
+    
+    # transformed image
+    T = np.zeros_like(src, np.uint8)
+
+    if args.m == "n":
+        for r in range(h):
+            for c in range(w):
+                for ch in range(src.shape[2]):
+                    if args.a != 0:
+                        x = round(c * rot_m[0][0] + r * rot_m[0][1] + rot_m[0][2])
+                        y = round(c * rot_m[1][0] + r * rot_m[1][1] + rot_m[1][2])
+                    elif args.s != 1.0 or args.d:
+                        x = round(c / sw)
+                        y = round(r / sh)
+
+                    if (0 <= x < w) and (0 <= y < h):
+                        T[r, c, ch] = src[y, x, ch]
+                    else:
+                        T[r, c, ch] = 255
+
+    elif args.m == "b":
+        for r in range(h):
+            for c in range(w):
+                for ch in range(src.shape[2]):
+                    if args.a != 0:
+                        x = c * rot_m[0][0] + r * rot_m[0][1] + rot_m[0][2]
+                        y = c * rot_m[1][0] + r * rot_m[1][1] + rot_m[1][2]
+                    elif args.s != 1.0 or args.d:
+                        x = c / sw
+                        y = r / sh
+
+                    if (0 <= x < w) and (0 <= y < h):
+                        T[r, c, ch] = bilinear(src, x, y, ch)
+                    else:
+                        T[r, c, ch] = 255
+
+    elif args.m == "bi":
+        for r in range(h):
+            for c in range(w):
+                for ch in range(src.shape[2]):
+                    if args.a != 0:
+                        x = c * rot_m[0][0] + r * rot_m[0][1] + rot_m[0][2]
+                        y = c * rot_m[1][0] + r * rot_m[1][1] + rot_m[1][2]
+                    elif args.s != 1.0 or args.d:
+                        x = c / sw
+                        y = r / sh
+
+                    if (0 <= x < w) and (0 <= y < h):
+                        T[r, c, ch] = bicubic(src, x, y, ch)
+                    else:
+                        T[r, c, ch] = 255
+                    
+    elif args.m == "l":
+        for r in range(h):
+            for c in range(w):
+                for ch in range(src.shape[2]):
+                    if args.a != 0:
+                        x = c * rot_m[0][0] + r * rot_m[0][1] + rot_m[0][2]
+                        y = c * rot_m[1][0] + r * rot_m[1][1] + rot_m[1][2]
+                    elif args.s != 1.0 or args.d:
+                        x = c / sw
+                        y = r / sh
+
+                    if (0 <= x < w) and (0 <= y < h):
+                        T[r, c, ch] = lagrange(src, x, y, ch)
+                    else:
+                        T[r, c, ch] = 255
+
+
+    else:
+        raise ValueError("Invalid interpolation method. Use 'n', 'b', 'bi', or 'l'.")
+
+    save(T, f"out-images/esc_{args.s}_{args.m}_{args.i.split("/")[-1]}")
 
 if __name__ == "__main__":
     main()
